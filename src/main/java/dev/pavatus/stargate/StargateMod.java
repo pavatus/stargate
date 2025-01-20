bacb741a2f90384d280cd5bd8aee2260897a957e
James Hall <james.hall01@outlook.com>
2025-01-20 20:50:16 +0000

    chore: template -> stargate

---
+++ src/main/java/dev/pavatus/stargate/StargateMod.java
@@ -0,0 +1,16 @@
+package dev.pavatus.stargate;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StargateMod implements ModInitializer {
+	public static final String MOD_ID = "stargate";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
