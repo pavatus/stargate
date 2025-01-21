4b0b18dcc72c8421b3e9f9df74cace86061eedb3
Loqor <65447472+Loqor@users.noreply.github.com>
2025-01-21 13:01:10 -0600

    fixed model

--- src/main/java/dev/pavatus/stargate/client/models/StargateModel.java
+++ src/main/java/dev/pavatus/stargate/client/models/StargateModel.java
@@ -17,9 +17,9 @@ public class StargateModel extends SinglePartEntityModel {
 		ModelData modelData = new ModelData();
 		ModelPartData modelPartData = modelData.getRoot();
 		ModelPartData gate = modelPartData.addChild("gate", ModelPartBuilder.create().uv(0, 81).cuboid(-8.0F, 72.2374F, -4.0F, 16.0F, 8.0F, 8.0F, new Dilation(0.01F))
-		.uv(147, 115).cuboid(-40.2187F, 32.0187F, -4.0F, 8.0F, 16.0F, 8.0F, new Dilation(0.01F))
-		.uv(147, 140).cuboid(32.2187F, 32.0187F, -4.0F, 8.0F, 16.0F, 8.0F, new Dilation(0.01F))
-		.uv(98, 132).cuboid(-8.0F, -0.2F, -4.0F, 16.0F, 8.0F, 8.0F, new Dilation(0.01F)), ModelTransform.pivot(0.0F, -56.2374F, 0.0F));
+				.uv(147, 115).cuboid(-40.2187F, 32.0187F, -4.0F, 8.0F, 16.0F, 8.0F, new Dilation(0.01F))
+				.uv(147, 140).cuboid(32.2187F, 32.0187F, -4.0F, 8.0F, 16.0F, 8.0F, new Dilation(0.01F))
+				.uv(98, 132).cuboid(-8.0F, -0.2F, -4.0F, 16.0F, 8.0F, 8.0F, new Dilation(0.01F)), ModelTransform.pivot(0.0F, -56.2374F, 0.0F));
 
 		ModelPartData cube_r1 = gate.addChild("cube_r1", ModelPartBuilder.create().uv(147, 98).cuboid(-8.0F, 0.0F, -4.0F, 16.0F, 8.0F, 8.0F, new Dilation(0.0F)), ModelTransform.of(-37.1572F, 24.6277F, 0.0F, 0.0F, 0.0F, -1.1781F));
 
@@ -45,7 +45,9 @@ public class StargateModel extends SinglePartEntityModel {
 
 		ModelPartData cube_r12 = gate.addChild("cube_r12", ModelPartBuilder.create().uv(49, 81).cuboid(-8.0F, -8.0F, -4.0F, 16.0F, 8.0F, 8.0F, new Dilation(0.0F)), ModelTransform.of(-15.391F, 77.176F, 0.0F, 0.0F, 0.0F, 0.3927F));
 
-		ModelPartData portal = gate.addChild("portal", ModelPartBuilder.create().uv(0, 0).cuboid(-40.0F, 0.0F, 0.0F, 80.0F, 80.0F, 0.0F, new Dilation(0.1F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+		ModelPartData portal = gate.addChild("portal", ModelPartBuilder.create().uv(0, 0).cuboid(-40.0F, -40.0F, 0.0F, 80.0F, 80.0F, 0.0F, new Dilation(0.1F))
+				.uv(0, 176).cuboid(-40.0F, -40.0F, -1.0F, 80.0F, 80.0F, 0.0F, new Dilation(0.1F))
+				.uv(0, 176).cuboid(-40.0F, -40.0F, 1.0F, 80.0F, 80.0F, 0.0F, new Dilation(0.1F)), ModelTransform.pivot(0.0F, 40.0F, 0.0F));
 		return TexturedModelData.of(modelData, 256, 256);
 	}
 	@Override
